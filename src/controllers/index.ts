16525f2efcd28ab7eb85a0b4848afd82fb7c29b7
Hugo Persson <hugopersson7@gmail.com>
2020-10-22 21:08:25 +0200

    Implemented middleware

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,7 @@
 import Controller from "../lib/Controller";
+import { addMiddleware, IMiddlewareFunction } from "@lib/Middleware"
+import Request from "@lib/Request";
+import Response from "@lib/Response";
 
 export default class Index extends Controller {
     public index(): void {
@@ -7,10 +10,21 @@ export default class Index extends Controller {
         );
     }
 
+    @addMiddleware([middlewareExample, middlewareExampleSecond])
     public newUser(): void {
         this.res.send("Create a new user here");
     }
     public getUserFile(): void {
         this.res.download(process.cwd() + "/resources/index.html");
     }
+
+
+}
+function middlewareExample(req: Request, res: Response, next: Function) {
+    console.log("WHOAH I AM A MIDDLEWARE");
+    next();
+}
+function middlewareExampleSecond(req: Request, res: Response, next: Function) {
+    console.log("WHOAH I AM ALSO A MIDDLEWARE");
+    next();
 }
--- src/lib/Middleware.ts
+++ src/lib/Middleware.ts
@@ -1,19 +1,27 @@
- // Method Decorator
-export function Confirmable(message: string) {
-  return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
+import Request from "@lib/Request";
+import Response from "@lib/Response";
+import Controller from "./Controller";
+
+// Method Decorator
+export function addMiddleware(stack: Array<IMiddlewareFunction>) {
+  return function (target: Controller, key: string | symbol, descriptor: PropertyDescriptor) {
     const original = descriptor.value;
-
-      descriptor.value = function( ... args: any[]) {
-          const allow = confirm(message);
-
-          if (allow) {
-            const result = original.apply(this, args);
-            return result;
-          } else {
-            return null;
-          }
+    descriptor.value = function (...args: any[]) {
+      executeMiddlewareStack(stack, target.res, target.req, 0);
+      return original.apply(this, args);
     };
-
     return descriptor;
   };
+}
+
+
+
+function executeMiddlewareStack(stack: Array<IMiddlewareFunction>, res: Response, req: Request, index: number) {
+  if (index < stack.length) stack[index](req, res, () => executeMiddlewareStack(stack, res, req, index + 1));
+}
+
+
+
+export interface IMiddlewareFunction {
+  (req: Request, res: Response, next: Function): void;
 }
