e24dc32e67be98588f5c03ebcd8d10d363590d34
Hugo Persson <hugo.e.persson@gmail.com>
2020-10-22 19:46:08 +0200

    ** Started some model and decorator work

--- src/lib/Model.ts
+++ src/lib/Model.ts
@@ -1,5 +1,10 @@
 
 export default abstract class Model {
-
-
+    /**
+     * get
+     * @param {object} filter - a object with keys and values after what you want to filet, for example {id:32,name:"hugo"} then server will return all rows that fufill this
+     */
+    public get(filter: object) {
+        // Will find a row dependent on the filter
+    }
 }
